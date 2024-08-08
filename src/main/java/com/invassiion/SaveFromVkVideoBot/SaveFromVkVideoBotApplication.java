3f5862e7f03548295efa0a1971cc8405acd8d790
invassiion <invassiion@gmail.com>
2024-08-08 11:32:46 +0700

    create project

---
+++ src/main/java/com/invassiion/SaveFromVkVideoBot/SaveFromVkVideoBotApplication.java
@@ -0,0 +1,13 @@
+package com.invassiion.SaveFromVkVideoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveFromVkVideoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveFromVkVideoBotApplication.class, args);
+	}
+
+}
